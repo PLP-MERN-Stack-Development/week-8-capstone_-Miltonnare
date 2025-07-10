7d848d59e3c6983a842319a32437b2583f92e206
Miltonnare <miltonnareblessed@gmail.com>
2025-07-10 14:35:46 +0300

    Created the Register Authentication

---
+++ server/controllers/authController.js
@@ -0,0 +1,59 @@
+const user=require('../models/userModel');
+const bycrypt=require('bcryptjs');
+const jwt=require('jsonwebtoken');
+
+
+const registerUser=async (req,res)=>{
+    try {
+        const {name,email,password,role,companyName}=req.body;
+
+        if(!name||!email||!password||!role){
+            return res.status(400).json({message:"All fields are required"});
+
+        }
+        const existingUser=await user.findOne({email})
+
+        if(existingUser){
+            return res.status(400).json({message:"User already exists"});
+
+        }
+
+        const hashedPassword=await bycrypt.hash(password,10);
+
+        const newUser=new user({
+            name,
+            email,
+            password:hashedPassword,
+            role,
+            companyName:role==='employer' ? companyName:undefined
+
+        })
+
+        await newUser.save();
+
+        const token=jwt.sign(
+            {id:user._id, role:user.role},
+           process.env.JWT_SECRET,
+           {expiresIn:'7d'}
+        );
+
+        res.status(201).json({
+            user:{
+                id:user._id,
+                name:user.name,
+               email:user.email,
+               role:user.role,
+               companyName:user.companyName
+            },
+            token
+        })
+
+       
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message:'Something went wrong'});
+        
+    }
+};
+
+module.exports={registerUser};
